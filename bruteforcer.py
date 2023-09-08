d31c864030300c22b1bce8d18eb762441c2b76be
unknown <ufukogutcu@gmail.com>
2023-09-08 12:40:42 +0100

    Added api providers

---
+++ bruteforcer.py
@@ -0,0 +1,11 @@
+api_providers = [
+    'https://blockchain.info/',
+    'https://www.bitgo.com/api/v1/',
+    'https://api.blockchair.com/bitcoin/',
+    'https://api.bitaps.com/btc/v1/',
+    'https://bitcoinblockexplorers.com/api/v1/',
+    'https://bitcoinblockexplorers.com/api/v2/',
+    'https://blockstream.info/api/',
+    'https://mempool.space/api/',
+    'https://harari.blocksmurfer.io/api/v1/btc/'
+    ]
